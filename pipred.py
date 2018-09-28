5dd98e0c1521e55b88a837be8a30c17b9d12cbe5
olekwk <aw370493@okwf.fuw.edu.pl>
2018-09-28 14:24:57 +0200

    add parsing PSSM from custom msa

--- pipred.py
+++ pipred.py
@@ -86,11 +86,15 @@ for entry, seq in zip(entries, sequences):
     if not get_pssm_sequence(pssm_fn) == seq:
         print("ERROR: Sequence in PSSM file does not match fasta sequence for entry %s!" % entry)
         exit()
-    try:
-        parsed_pssm = np.genfromtxt(pssm_fn, skip_header=3, skip_footer=5, usecols=(i for i in range(2, 22)))
-    except ValueError:
-        print("ERROR: Malformed PSSM file for entry %s!" % entry)
-        exit()
+        try:
+            parsed_pssm = np.genfromtxt(pssm_fn, skip_header=3, skip_footer=5, usecols=(i for i in range(2, 22)))
+            if not parsed_pssm.shape[0] == len(seq):
+                parsed_pssm = np.genfromtxt(pssm_fn, skip_header=3, skip_footer=3, usecols=(i for i in range(2, 22)))
+                if not parsed_pssm.shape[0] == len(seq):
+                    raise ValueError
+        except ValueError:
+            print("ERROR: Malformed PSSM file for entry %s!" % entry)
+            exit()
     
     if not parsed_pssm.shape[0] == len(seq) and parsed_pssm.shape[1] == 20:
         if parsed_pssm.shape[0] == len(seq)-2:
