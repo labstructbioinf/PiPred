4b45f4fedd2aaf3c9f8609a598ee1fe72762a273
Jan Ludwiczak <j.ludwiczak@cent.uw.edu.pl>
2018-09-30 18:45:07 +0200

    Revert "change decoding prediction function"
    This reverts commit 318a47d63019ade5e20cb846be1db2f7861f85c8.

--- utils.py
+++ utils.py
@@ -72,7 +72,11 @@ def enc_pssm(pssm_file, pad_length=None, pad_left=0):
 
 # Decodes predictions (takes into the account padding of sequence)
 def decode(pred, enc_seq):
-    return np.delete(pred, np.where(~enc_seq.any(axis=1)), axis=0)[:,1]
+    decoded_pred = []
+    for pos_pred, pos_seq in zip(pred, enc_seq):
+        if not np.array_equal(pos_seq, np.zeros(40)):
+            decoded_pred.append(pos_pred)
+    return np.asarray(decoded_pred)
 
 # Pipred model architecture
 def PiPred_Model():
